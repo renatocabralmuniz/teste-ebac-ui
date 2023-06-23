a9be240f84731eb3d7649309711136134329eeeb
renatocabralmuniz <renatocabralmuniz@gmail.com>
2023-06-23 16:51:05 -0300

    Teste de Cadastro de Cliente e Edição

---
+++ cypress/e2e/pre-cadastro.cy.js
@@ -0,0 +1,29 @@
+/// <reference types="cypress"/>
+import { faker } from '@faker-js/faker';
+
+describe('Funcionalidade Pré Cadastro', () => {
+
+    beforeEach(() => {
+        cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
+    });
+
+    afterEach(() => {
+        cy.screenshot()
+    });
+
+    it('Deve completar o pre cadastro com sucesso', () => {
+        cy.get('#reg_email').type(faker.internet.email())
+        cy.get('#reg_password').type(faker.internet.password())
+        cy.get(':nth-child(4) > .button').click()
+
+        cy.get('.woocommerce-MyAccount-navigation-link--edit-account > a').click()
+        cy.get('#account_first_name').type(faker.person.firstName())
+        cy.get('#account_last_name').type(faker.person.lastName())
+        cy.get('#account_display_name').type(faker.person.firstName())
+        cy.get('.woocommerce-Button').click()
+        
+        cy.get('.woocommerce-message').should('contain', 'Detalhes da conta modificados')
+
+    });
+    
+});
