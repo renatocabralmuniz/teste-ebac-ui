7e4412798f6dcd83fef48bc8f91b1766525c012a
renatocabralmuniz <renatocabralmuniz@gmail.com>
2023-07-26 12:31:41 -0300

    Alterações - Geração dinamica de Email e Senha

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,4 +1,5 @@
 /// <reference types="cypress"/>
+import { faker } from '@faker-js/faker';
 
 context('Funcionalidade Login', () => {
 
@@ -10,6 +11,9 @@ context('Funcionalidade Login', () => {
         cy.screenshot()
     });
 
+    let emailFaker = faker.internet.email()
+    let senhaFaker = faker.internet.password()
+
     it ('Deve fazer login com sucesso',  () => {
         cy.get('#username').type('aluno_ebac@teste.com')
         cy.get('#password').type('teste@teste.com')
@@ -20,16 +24,16 @@ context('Funcionalidade Login', () => {
     })
 
     it('Deve exibir uma mensagem de erro ao inserir usuário inválido', () => {
-        cy.get('#username').type('ebac@teste.com')
+        cy.get('#username').type(emailFaker)
         cy.get('#password').type('teste@teste.com')
         cy.get('.woocommerce-form > .button').click()
 
-        cy.get('.woocommerce-error').should('contain', 'Erro: a senha fornecida para o e-mail ebac@teste.com está incorreta. Perdeu a senha?')
+        cy.get('.woocommerce-error').should('contain', 'Endereço de e-mail desconhecido. Verifique novamente ou tente seu nome de usuário.')
     })
 
     it('Deve exibir uma mensagem de erro ao inserir senha inválida', () => {
         cy.get('#username').type('aluno_ebac@teste.com')
-        cy.get('#password').type('teste@teste')
+        cy.get('#password').type(senhaFaker)
         cy.get('.woocommerce-form > .button').click()
 
         cy.get('.woocommerce-error').should('contain', 'Erro: a senha fornecida para o e-mail aluno_ebac@teste.com está incorreta. Perdeu a senha?')
