e65d1fda1e77f374688d000d1744d438ea2d5343
renatocabralmuniz <renatocabralmuniz@gmail.com>
2023-08-16 17:03:52 -0300

    Implementação de Endereço de Entrega com Massa de dados e Refatoração do Endereço de Faturamento com Massa de dados

--- cypress/e2e/endereco.cy.js
+++ cypress/e2e/endereco.cy.js
@@ -1,7 +1,8 @@
 /// <reference types="cypress"/>
 import enderecoPageCy from "../support/page-objects/endereco.page.cy";
 import { faker } from '@faker-js/faker';
-const dadosEndereco = require('../fixtures/endereco.json')
+const dadosEnderecoFaturamento = require('../fixtures/enderecoFaturamento.json')
+const dadosEnderecoEntrega = require('../fixtures/enderecoEntrega.json')  
 
 describe('Funcionalidade Endereços - Faturamento e Entrega', () => {
     beforeEach(() => {
@@ -18,21 +19,40 @@ describe('Funcionalidade Endereços - Faturamento e Entrega', () => {
 
     
     it ('Deve fazer cadastro de faturamento com sucesso', () => {
-        enderecoPageCy.editarEnderecoFaturamento('Renato', 'Muniz', 'Google', 'Brasil', faker.location.street(), faker.location.buildingNumber(), faker.location.city(), 'Rio de Janeiro', '24425180')
+        enderecoPageCy.editarEnderecoFaturamento('Renato', 'Muniz', 'Google', 'Brasil', faker.location.street(), faker.location.buildingNumber(), faker.location.city(), 'Rio de Janeiro', '24425180', '21982825544', 'renatomuniz@email.com')
         cy.get('.woocommerce-message').should('contain', 'Endereço alterado com sucesso.')
     });
 
-    it.only ('Deve fazer cadastro de faturamento com sucesso - Usando arquivo de dados', () => {
+    it ('Deve fazer cadastro de faturamento com sucesso - Usando arquivo de dados', () => {
         enderecoPageCy.editarEnderecoFaturamento(
-            dadosEndereco[1].nome,
-            dadosEndereco[1].sobrenome,
-            dadosEndereco[1].empresa,
-            dadosEndereco[1].pais,
-            dadosEndereco[1].endereco,
-            dadosEndereco[1].numero,
-            dadosEndereco[1].cidade,
-            dadosEndereco[1].estado,
-            dadosEndereco[1].cep
+            dadosEnderecoFaturamento[0].nome,
+            dadosEnderecoFaturamento[0].sobrenome,
+            dadosEnderecoFaturamento[0].empresa,
+            dadosEnderecoFaturamento[0].pais,
+            dadosEnderecoFaturamento[0].endereco,
+            dadosEnderecoFaturamento[0].numero,
+            dadosEnderecoFaturamento[0].cidade,
+            dadosEnderecoFaturamento[0].estado,
+            dadosEnderecoFaturamento[0].cep,
+            dadosEnderecoFaturamento[0].telefone,
+            dadosEnderecoFaturamento[0].email
+            
+            )
+        cy.get('.woocommerce-message').should('contain', 'Endereço alterado com sucesso.')
+    });
+
+    it ('Deve fazer cadastro de entrega com sucesso - Usando arquivo de dados', () => {
+        enderecoPageCy.editarEnderecoEntrega(
+            dadosEnderecoEntrega[2].nome,
+            dadosEnderecoEntrega[2].sobrenome,
+            dadosEnderecoEntrega[2].empresa,
+            dadosEnderecoEntrega[2].pais,
+            dadosEnderecoEntrega[2].endereco,
+            dadosEnderecoEntrega[2].numero,
+            dadosEnderecoEntrega[2].cidade,
+            dadosEnderecoEntrega[2].estado,
+            dadosEnderecoEntrega[2].cep
+            
             )
         cy.get('.woocommerce-message').should('contain', 'Endereço alterado com sucesso.')
     });
