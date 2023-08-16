bb71ddbb6cab3e586459c40ed4ff1fa6bbdfa0cd
renatocabralmuniz <renatocabralmuniz@gmail.com>
2023-08-16 16:17:47 -0300

    Implementação de Massa de Dados em Lista

--- cypress/e2e/endereco.cy.js
+++ cypress/e2e/endereco.cy.js
@@ -1,6 +1,7 @@
 /// <reference types="cypress"/>
 import enderecoPageCy from "../support/page-objects/endereco.page.cy";
 import { faker } from '@faker-js/faker';
+const dadosEndereco = require('../fixtures/endereco.json')
 
 describe('Funcionalidade Endereços - Faturamento e Entrega', () => {
     beforeEach(() => {
@@ -18,6 +19,21 @@ describe('Funcionalidade Endereços - Faturamento e Entrega', () => {
     
     it ('Deve fazer cadastro de faturamento com sucesso', () => {
         enderecoPageCy.editarEnderecoFaturamento('Renato', 'Muniz', 'Google', 'Brasil', faker.location.street(), faker.location.buildingNumber(), faker.location.city(), 'Rio de Janeiro', '24425180')
+        cy.get('.woocommerce-message').should('contain', 'Endereço alterado com sucesso.')
+    });
+
+    it.only ('Deve fazer cadastro de faturamento com sucesso - Usando arquivo de dados', () => {
+        enderecoPageCy.editarEnderecoFaturamento(
+            dadosEndereco[1].nome,
+            dadosEndereco[1].sobrenome,
+            dadosEndereco[1].empresa,
+            dadosEndereco[1].pais,
+            dadosEndereco[1].endereco,
+            dadosEndereco[1].numero,
+            dadosEndereco[1].cidade,
+            dadosEndereco[1].estado,
+            dadosEndereco[1].cep
+            )
         cy.get('.woocommerce-message').should('contain', 'Endereço alterado com sucesso.')
     });
 });
